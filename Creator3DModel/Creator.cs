4f55f6ebc84825c2fcd349dd3253558d0ce516ac
6N2P <umnik1985@mail.ru>
2023-04-17 09:05:29 +0500

    first commit

---
+++ Creator3DModel/Creator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Creator3DModel
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Creator" в коде и файле конфигурации.
+    public class Creator : ICreator
+    {
+        public int  CreateCub(int height, int width, int thickness)
+        {
+            //создать метод для возвращения куба
+            return height * width * thickness;
+        }
+    }
+}
---
+++ Creator3DModel/ICreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Creator3DModel
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "ICreator" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface ICreator
+    {
+        [OperationContract]
+        int CreateCub(int height, int width, int thickness);
+    }
+}
